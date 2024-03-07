87bdeb5ad9f023f76e45a8a22b2450cefebf3d5d
IvanCallisaya <ivanedcali@gmail.com>
2024-03-07 18:57:33 -0400

    segundo commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'package:flutter/material.dart';
+
+import 'package:translator_test/Translator.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TranslatorApp()
+    ), // use MaterialApp
+  );
+}
